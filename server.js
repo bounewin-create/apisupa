1823c938ed16f56708ea850f8354fb69134ef587
bounewin <bounewin@gmail.com>
2026-03-23 18:57:19 +0000

    first commit

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ import express from "express";
 import usersRouter from "./routes/users.js";
 
 const app = express();
-app.use(express.json());
+//app.use(express.json());
 
-// const cors = require("cors");
-// app.use(cors());
+const cors = require("cors");
+app.use(cors());
 
 // Utilisation du router "users"
 app.use("/users", usersRouter);
